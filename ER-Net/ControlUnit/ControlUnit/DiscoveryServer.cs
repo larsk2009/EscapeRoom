93962e6f5285bd66dea5189471c9fdaad2f29492
Lars Koopmans <larsk2009@live.nl>
2019-03-24 12:17:52 +0100

    New Project PuzzleClient and ControlUnit as examples for ER-Net. PuzzleClient doesn't build yet.
    Signed-off-by: Lars Koopmans <larsk2009@live.nl>

---
+++ ER-Net/ControlUnit/ControlUnit/DiscoveryServer.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ControlUnit
+{
+    public class DiscoveryServer
+    {
+        private UdpClient _client;
+        private const int DiscoveryPort = 49666;
+
+
+        public DiscoveryServer()
+        {
+            _client = new UdpClient
+            {
+                EnableBroadcast = true,
+                MulticastLoopback = true
+            };
+        }
+
+        public void DoDiscovery()
+        {
+            var data = new
+            {
+                Guid = Guid.NewGuid().ToString(),
+                MessageType = "Discovery"
+            };
+            SendUdpData(data);
+        }
+
+        private void SendUdpData(object data)
+        {
+            var json = "ER-NET\n" + JsonConvert.SerializeObject(data);
+            var bytes = Encoding.ASCII.GetBytes(json);
+
+            IPEndPoint broadCastEndPoint = new IPEndPoint(IPAddress.Broadcast, DiscoveryPort);
+
+            _client.Send(bytes, bytes.Length, broadCastEndPoint);
+        }
+    }
+}
