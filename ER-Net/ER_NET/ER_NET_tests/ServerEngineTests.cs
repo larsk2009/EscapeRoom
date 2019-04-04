23e5059622fb054d8d07883eb188ece86900c1c4
Lars Koopmans <larsk2009@live.nl>
2019-04-04 12:37:36 +0200

    - Added some unit tests for more coverage
    Signed-off-by: Lars Koopmans <larsk2009@live.nl>

--- ER-Net/ER_NET/ER_NET_tests/ServerEngineTests.cs
+++ ER-Net/ER_NET/ER_NET_tests/ServerEngineTests.cs
@@ -43,5 +43,38 @@ namespace ER_NET_tests
 
             Assert.False(engine.IsDeviceConnected(guid));
         }
+
+        [Fact]
+        void NullMessageTest()
+        {
+            var parser = new MockTcpParser();
+            var engine = new ErNetServerEngine(parser);
+
+            var guid = Guid.NewGuid();
+            var eventArgs = new TcpEventArgs(null, IPAddress.Parse("192.168.2.10"));
+            parser.RaiseTcpEvent(eventArgs);
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            Assert.False(engine.IsDeviceConnected(guid));
+        }
+
+        [Fact]
+        void UpdateTimeTest()
+        {
+            var parser = new MockTcpParser();
+            var engine = new ErNetServerEngine(parser);
+
+            Guid guid = Guid.Parse("e57d5d61-4e98-42b3-a096-81fd249b2cef");
+            var testJson = $"ER-NET\n{{\"Guid\":\"{guid}\",\"MessageType\":\"DiscoveryAcknowledge\"}}";
+            var eventArgs = new TcpEventArgs(Message.FromJson(testJson), IPAddress.Parse("192.168.2.10"));
+
+            //Run 3 times to see if the time gets updated
+            for (int i = 0; i < 3; i++)
+            {
+                parser.RaiseTcpEvent(eventArgs);
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+            }
+            
+            Assert.True(engine.IsDeviceConnected(guid));
+        }
     }
 }
